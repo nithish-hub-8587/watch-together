fcf05fa68fb941ae9fb9c4b8d5ee562bdf10b774
Nithish <nithish8587@gmail.com>
2026-02-27 22:53:07 +0530

    Render fix

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+const PORT = process.env.PORT || 3000;
 
 io.on("connection", (socket) => {
     socket.on("join-room", (roomID) => {
