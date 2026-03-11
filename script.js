16650e8bea593fd6614a379ac95ecb7b334491ab
Nithish <nithish8587@gmail.com>
2026-03-11 17:20:48 +0530

    Fix laptop connection

--- script.js
+++ script.js
@@ -21,22 +21,29 @@ fullscreenBtn.addEventListener("click", () => {
 
 const config = {
     iceServers: [
-        { urls: "stun:stun.l.google.com:19302" },
-
+        {
+            urls: "stun:stun.l.google.com:19302"
+        },
+        {
+            urls: "stun:stun1.l.google.com:19302"
+        },
         {
             urls: "turn:openrelay.metered.ca:80",
             username: "openrelayproject",
             credential: "openrelayproject"
         },
-
         {
             urls: "turn:openrelay.metered.ca:443",
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        },
+        {
+            urls: "turn:openrelay.metered.ca:443?transport=tcp",
             username: "openrelayproject",
             credential: "openrelayproject"
         }
     ]
 };
-
 // ================= CREATE ROOM =================
 createBtn.addEventListener("click", async () => {
 
